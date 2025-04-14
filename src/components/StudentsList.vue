cd663fcda1f7b40ccc966dae9f347644e96532c3
Pcuong2406112005 <phanmanhcuong2411@gmail.com>
2025-04-14 22:56:55 +0700

    Cập nhật App.vue, xóa HelloWorld.vue và thêm StudentsList.vue

---
+++ src/components/StudentsList.vue
@@ -0,0 +1,18 @@
+<script setup>
+    defineProps({
+        lopName: String,
+        students: Array,
+        nganhindex: Number,
+        lopindex:Number,
+    });
+</script>
+<template>
+<template v-for="(student,index) in students" :key="index">
+     <tr>
+        <td>{{ (nganhindex+1)+'.'+(lopindex+1)+'.'+(index+1) }}</td>
+        <td>{{ student.name }}</td>
+        <td>{{ lopName }}</td>
+        <td></td>
+    </tr>
+</template>
+</template>
